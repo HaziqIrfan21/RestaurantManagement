9520a2d76e2bdc58a7eda2499b40e723f5b77449
HaziqIrfan21 <90903576+HaziqIrfan21@users.noreply.github.com>
2021-10-27 10:38:04 +0800

    updated DB file comment
    - Updated the DB file comments

--- Haziq_FinalProject/DB.cs
+++ Haziq_FinalProject/DB.cs
@@ -9,7 +9,7 @@ namespace Haziq_FinalProject
     {
         private MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;username=root;password=;email=;database=finalproject_users_db;SSL Mode=None");
 
-        //open connection
+        //open connection to my sql server
         public void OpenConnection()
         {
             if(connection.State == System.Data.ConnectionState.Closed)
